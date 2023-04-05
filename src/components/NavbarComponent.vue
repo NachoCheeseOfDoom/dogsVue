10d00e740ab420223526b873b8dddcb9c767989e
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2023-04-05 09:26:59 -0600

    nav bar color

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <template> 
-<nav  class="navbar navbar-light bg-dark"> 
+<nav id="navbar-color" class="navbar "> 
     <img src="../assets/images/pawprints.png" width="40" alt="pawprint"> 
 </nav>
 
@@ -12,4 +12,10 @@ export default {
 </script>
 
 <style>
+
+#navbar-color{ 
+  background: rgb(33,37,41);
+background: radial-gradient(circle, rgba(33,37,41,0.908000700280112) 0%, rgba(33,37,41,0.8155637254901961) 100%);
+}
+
 </style>
